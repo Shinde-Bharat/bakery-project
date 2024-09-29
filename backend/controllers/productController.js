a53d3978cf9f025a70e02b08756ba4696b5956a1
Rohit Nandagawali <nandagawalirohit143@gmail.com>
2024-09-29 12:10:53 +0530

    recommedation done

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -15,6 +15,7 @@ exports.createProduct = async (req, res) => {
 
         const product = new Product({
             name: req.body.name,
+            description: req.body.description,
             category: req.body.category,
             price: req.body.price,
             avlQuantity: req.body.avlQuantity,
@@ -38,7 +39,10 @@ exports.getAllProducts = async (req, res) => {
 
 exports.getProduct = async (req, res) => {
     try {
-        const product = await Product.findById(req.params.id).populate('category');
+        const product = await Product.findById(req.params.id).populate('category').populate({
+            path: 'reviews.user', // Populating user details in reviews
+            select: 'name'        // Only selecting the name field from User
+        });
         if (!product) return res.status(404).json({ message: 'Product not found' });
         res.json(product);
     } catch (error) {
@@ -67,3 +71,57 @@ exports.deleteProduct = async (req, res) => {
 };
 
 
+
+exports.addReview = async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id);
+        if (!product) return res.status(404).json({ message: 'Product not found' });
+        console.log(req.user);
+
+
+        const newReview = {
+            user: req.user.userId,
+            text: req.body.text,
+            rating: req.body.rating
+        };
+
+        product.reviews.push(newReview);
+        product.averageRating = product.calculateAverageRating();
+        await product.save();
+
+        res.status(201).json(product);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
+
+exports.getRecommendations = async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id).populate('category');
+        if (!product) return res.status(404).json({ message: 'Product not found' });
+
+        // Get products from the same category
+        const sameCategory = await Product.find({
+            category: product.category._id,
+            _id: { $ne: product._id }
+        }).limit(4).populate({
+            path: 'category',
+            select: 'name'
+        });
+
+        // Get products from other categories
+        const otherCategories = await Product.find({
+            category: { $ne: product.category._id },
+            _id: { $ne: product._id }
+        }).limit(4).populate({
+            path: 'category',
+            select: 'name'
+        });
+
+        const recommendations = [...sameCategory, ...otherCategories];
+
+        res.json(recommendations);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
